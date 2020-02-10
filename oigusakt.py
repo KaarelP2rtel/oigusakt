3c182c71f964c5736913df2d825bb57459c82481
Kaarel Pärtel <kaarelp2rtel@gmail.com>
2020-02-10 20:51:02 +0200

    Add more of the DS
    Now has most of the datastructure  needed.

--- oigusakt.py
+++ oigusakt.py
@@ -2,6 +2,7 @@
 
 import xml.etree.ElementTree as ElementTree
 from cached_property import cached_property
+from pydoc import locate
 
 def cached_element_text(func):
     @cached_property
@@ -15,6 +16,12 @@ def cached_element_attribute(func):
         return context._element.attrib.get(func.__name__)
     return findAttribute
 
+def cached_element(func):
+    @cached_property
+    def findElement(context):
+        return eval(func.__name__.capitalize())(context.findChild(func.__name__))
+    return findElement
+
 
 class BaseElement():
 
@@ -25,10 +32,12 @@ class BaseElement():
     def findChild(self,name):
         return self._element.find('ts:{}'.format(name),namespaces=self._ns)
 
+    def findChildren(self,name):
+        return self._element.findall('ts:{}'.format(name),namespaces=self._ns)
+
 
     def attrib(self,name):
         return self
-
 
 class Oigusakt(BaseElement):
     def __init__(self,xml):
@@ -38,9 +47,26 @@ class Oigusakt(BaseElement):
     def id(self):
         pass
 
-    @cached_property
+    @cached_element
     def metaandmed(self):
-        return Metaandmed(self.findChild('metaandmed'))
+        pass
+
+    @cached_element
+    def aktinimi(self):
+        pass
+
+    @cached_property
+    def muutmismarkmed(self):
+        return tuple(Muutmismarge(i) for i in self.findChildren('muutmismarge'))
+
+    @cached_element
+    def sisu(self):
+        pass
+
+    @cached_element
+    def normtehnmarkus(self):
+        pass
+
 
 
 class Metaandmed(BaseElement):
@@ -68,9 +94,17 @@ class Metaandmed(BaseElement):
     def dokumentStaatus(self):
         pass
 
-    @cached_property
+    @cached_element
     def vastuvoetud(self):
-        return Vastuvoetud(self.findChild('vastuvoetud'))
+        pass
+    @cached_element
+    def avaldamismarge(self):
+        pass
+    @cached_element
+    def kehtivus(self):
+        pass
+
+
 
 class Vastuvoetud(BaseElement):
     @cached_element_text
@@ -79,4 +113,167 @@ class Vastuvoetud(BaseElement):
 
     @cached_element_text
     def joustumine(self):
+        pass
+
+    @cached_element
+    def avaldamismarge(self):
+        pass
+
+class Avaldamismarge(BaseElement):
+
+    @cached_element_text
+    def RTosa(self):
+        pass
+    @cached_element_text
+    def RTaasta(self):
+        pass
+    @cached_element_text
+    def RTnr(self):
+        pass
+    @cached_element_text
+    def RTartikkel(self):
+        pass
+    @cached_element_text
+    def aktViide(self):
+        pass
+    @cached_element_text
+    def avaldamineKuupaev(self):
+        pass
+
+class Kehtivus(BaseElement):
+    @cached_element_text
+    def kehtivuseAlgus(selfl):
+        pass
+
+class Aktinimi(BaseElement):
+    @cached_element
+    def nimi(self):
+        pass
+
+
+class Nimi(BaseElement):
+    @cached_element_text
+    def pealkiri(self):
+        pass
+    @cached_element
+    def normtehnmarkus(self):
+        pass
+
+class Normtehnmarkus(BaseElement):
+    @cached_element_text
+    def normtehnmarkusNr(self):
+        pass
+
+    @cached_element_text
+    def normtehnmarkusTekst(self):
+        pass
+
+    @cached_element
+    def muutmismarge(self):
+        pass
+
+
+class Muutmismarge(BaseElement):
+    @cached_element_text
+    def aktikuupaev(self):
+        pass
+
+    @cached_element
+    def avaldamismarge(self):
+        pass
+    @cached_element_text
+    def joustumine(self):
+        pass
+
+    @cached_element_text
+    def tavatekst(self):
+        pass
+
+class Sisu(BaseElement):
+    @cached_property
+    def peatykid(self):
+        return tuple(Peatykk(i) for i in self.findChildren('peatykk'))
+
+
+class Peatykk(BaseElement):
+    @cached_element_attribute
+    def id(self):
+        pass
+    @cached_element_text
+    def peatykkNr(self):
+        pass
+    @cached_element_text
+    def kuvatavNr(self):
+        pass
+    @cached_element_text
+    def peatykkPealkiri(self):
+        pass
+
+    @cached_property
+    def paragrahvid(self):
+        return tuple(Paragrahv(i) for i in self.findChildren('paragrahv'))
+
+class Paragrahv(BaseElement):
+    @cached_element_attribute
+    def id(self):
+        pass
+    @cached_element_text
+    def kuvatavNr(self):
+        pass
+    @cached_element_text
+    def paragrahvPealkiri(self):
+        pass
+    @cached_element
+    def muutmismarge(self):
+        pass
+
+    @cached_property
+    def loiked(self):
+        return tuple(Loige(i) for i in self.findChildren('loige'))
+
+class Loige(BaseElement):
+
+    @cached_element_attribute
+    def id(self):
+        pass
+
+    @cached_element_text
+    def loigeNr(self):
+        pass
+
+    @cached_element_text
+    def kuvatavNr(self):
+        pass
+
+    @cached_element
+    def muutmismarge(self):
+        pass
+
+    @cached_element
+    def sisuTekst(self):
+        pass
+
+    @cached_property
+    def alampunktid(self):
+        return tuple(Alampunkt(i) for i in self.findChildren('alampunkt'))
+class Sisutekst(BaseElement):
+
+    @cached_element_text
+    def tavatekst(self):
+        pass
+
+class Alampunkt(BaseElement):
+
+    @cached_element_attribute
+    def id(self):
+        pass
+
+    @cached_element_text
+    def alampunktNr(self):
+        pass
+    @cached_element_text
+    def kuvatavNr(self):
+        pass
+    @cached_element
+    def sisuTekst(self):
         pass
--- tests/test.py
+++ tests/test.py
@@ -5,14 +5,14 @@ from oigusakt import *
 
 class TestOigusakt(unittest.TestCase):
     def setUp(self):
-        testxml="tests/relvaseadus.xml"
-        xml = open(testxml,'r')
-        self.akt = Oigusakt(xml=xml)
+        testxml='tests/relvaseadus.xml'
+        with open(testxml,'r') as xml:
+            self.akt = Oigusakt(xml=xml)
 
 
     def test_oigusakt_has_id(self):
 
-        self.assertEqual("7bd95b30-0712-4479-b382-c53a8038faf5",self.akt.id)
+        self.assertEqual('7bd95b30-0712-4479-b382-c53a8038faf5',self.akt.id)
 
     def test_oigusakt_has_metaandmed(self):
 
@@ -29,12 +29,63 @@ class TestOigusakt(unittest.TestCase):
 
     def test_metaandmed_has_vastuvoetud(self):
         self.assertIsInstance(self.akt.metaandmed.vastuvoetud,Vastuvoetud)
+    def test_metaandmed_has_avaldamismarge(self):
+        self.assertIsInstance(self.akt.metaandmed.avaldamismarge,Avaldamismarge)
 
     def test_vastuvoetud_has_fields(self):
         vastuvoetud= self.akt.metaandmed.vastuvoetud
         self.assertEqual('2001-06-13',vastuvoetud.aktikuupaev)
         self.assertEqual('2002-03-31',vastuvoetud.joustumine)
 
+    def test_vastuvoetud_has_avaldamismarge(self):
+        self.assertIsInstance(self.akt.metaandmed.vastuvoetud.avaldamismarge,Avaldamismarge)
+
+    def test_vastuvoetud_avaldamismarge_has_fields(self):
+        am = self.akt.metaandmed.vastuvoetud.avaldamismarge
+        self.assertEqual('RT I',am.RTosa)
+        self.assertEqual('2001',am.RTaasta)
+        self.assertEqual('65',am.RTnr)
+        self.assertEqual('377',am.RTartikkel)
+        self.assertEqual('73058',am.aktViide)
+
+    def test_meta_avaldamismarge_has_fields(self):
+        am = self.akt.metaandmed.avaldamismarge
+        self.assertEqual('RT I',am.RTosa)
+        self.assertEqual('2019',am.RTaasta)
+        self.assertEqual('80',am.RTartikkel)
+        self.assertEqual('119032019080',am.aktViide)
+        self.assertEqual('2019-03-19+02:00',am.avaldamineKuupaev)
+    def test_meta_has_kehtivus(self):
+        self.assertIsInstance(self.akt.metaandmed.kehtivus,Kehtivus)
+
+    def test_meta_kehtivus_has_fields(self):
+        kehtivus = self.akt.metaandmed.kehtivus
+        self.assertEqual('2020-01-01',kehtivus.kehtivuseAlgus)
+
+    #At this point I decided to skip writing the same test with different names.
+
+    def test_oigusakt_has_name(self):
+        self.assertEqual('Relvaseadus',self.akt.aktinimi.nimi.pealkiri )
+
+    def test_oigusakt_has_normtehnmarkus(self):
+        self.assertEqual('1',self.akt.aktinimi.nimi.normtehnmarkus.normtehnmarkusNr )
+        self.assertIsInstance(self.akt.muutmismarkmed,tuple)
+        self.assertIsInstance(self.akt.muutmismarkmed[0],Muutmismarge)
+
+    def test_oigusakt_has_muutmismarkmed(self):
+        self.assertEqual(61,len(self.akt.muutmismarkmed))
+    def test_muutmismarge_has_fields(self):
+        self.assertEqual('531',self.akt.muutmismarkmed[0].avaldamismarge.RTartikkel)
+        self.assertEqual(', osaliselt 14.12.2019',self.akt.muutmismarkmed[59].tavatekst)
+
+    def test_akt_has_peatykid(self):
+        self.assertEqual(20,len(self.akt.sisu.peatykid))
+
+    def test_peatykk_has_paragrahvs(self):
+        self.assertEqual(14,len(self.akt.sisu.peatykid[0].paragrahvid))
+
+    def test_paragrahv_has_loiked(self):
+        self.assertEqual(5,len(self.akt.sisu.peatykid[0].paragrahvid[0].loiked))
 
 
 
